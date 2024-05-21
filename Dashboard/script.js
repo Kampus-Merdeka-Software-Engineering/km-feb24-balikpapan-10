7f85e4f6deecffc2b93486fc30e40ee2ea134b6f
Nadya Ramadhani Efendi <nadya.ramadhanie11@gmail.com>
2024-05-21 09:06:04 +0700

    new dashboard

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,188 +1,142 @@
-// SIDEBAR DROPDOWN
-
-
-allDropdown.forEach(item=> {
-	const a = item.parentElement.querySelector('a:first-child');
-	a.addEventListener('click', function (e) {
-		e.preventDefault();
-
-		if(!this.classList.contains('active')) {
-			allDropdown.forEach(i=> {
-				const aLink = i.parentElement.querySelector('a:first-child');
-
-				aLink.classList.remove('active');
-				i.classList.remove('show');
-			})
-		}
-
-		this.classList.toggle('active');
-		item.classList.toggle('show');
-	})
-})
-
-
-// SIDEBAR COLLAPSE
-const toggleSidebar = document.querySelector('nav .toggle-sidebar');
-const allSideDivider = document.querySelectorAll('#sidebar .divider');
-
-if(sidebar.classList.contains('hide')) {
-	allSideDivider.forEach(item=> {
-		item.textContent = '-'
-	})
-	allDropdown.forEach(item=> {
-		const a = item.parentElement.querySelector('a:first-child');
-		a.classList.remove('active');
-		item.classList.remove('show');
-	})
-} else {
-	allSideDivider.forEach(item=> {
-		item.textContent = item.dataset.text;
-	})
-}
-
-toggleSidebar.addEventListener('click', function () {
-	sidebar.classList.toggle('hide');
-
-	if(sidebar.classList.contains('hide')) {
-		allSideDivider.forEach(item=> {
-			item.textContent = '-'
-		})
-
-		allDropdown.forEach(item=> {
-			const a = item.parentElement.querySelector('a:first-child');
-			a.classList.remove('active');
-			item.classList.remove('show');
-		})
-	} else {
-		allSideDivider.forEach(item=> {
-			item.textContent = item.dataset.text;
-		})
-	}
-})
-
-
-sidebar.addEventListener('mouseleave', function () {
-	if(this.classList.contains('hide')) {
-		allDropdown.forEach(item=> {
-			const a = item.parentElement.querySelector('a:first-child');
-			a.classList.remove('active');
-			item.classList.remove('show');
-		})
-		allSideDivider.forEach(item=> {
-			item.textContent = '-'
-		})
-	}
-})
-sidebar.addEventListener('mouseenter', function () {
-	if(this.classList.contains('hide')) {
-		allDropdown.forEach(item=> {
-			const a = item.parentElement.querySelector('a:first-child');
-			a.classList.remove('active');
-			item.classList.remove('show');
-		})
-		allSideDivider.forEach(item=> {
-			item.textContent = item.dataset.text;
-		})
-	}
-})
-
-
-
-
-// PROFILE DROPDOWN
-const profile = document.querySelector('nav .profile');
-const imgProfile = profile.querySelector('img');
-const dropdownProfile = profile.querySelector('.profile-link');
-
-imgProfile.addEventListener('click', function () {
-	dropdownProfile.classList.toggle('show');
-})
-
-
-
-
-// MENU
-const allMenu = document.querySelectorAll('main .content-data .head .menu');
-
-allMenu.forEach(item=> {
-	const icon = item.querySelector('.icon');
-	const menuLink = item.querySelector('.menu-link');
-
-	icon.addEventListener('click', function () {
-		menuLink.classList.toggle('show');
-	})
-})
-
-
-
-window.addEventListener('click', function (e) {
-	if(e.target !== imgProfile) {
-		if(e.target !== dropdownProfile) {
-			if(dropdownProfile.classList.contains('show')) {
-				dropdownProfile.classList.remove('show');
-			}
-		}
-	}
-
-	allMenu.forEach(item=> {
-		const icon = item.querySelector('.icon');
-		const menuLink = item.querySelector('.menu-link');
-
-		if(e.target !== icon) {
-			if(e.target !== menuLink) {
-				if (menuLink.classList.contains('show')) {
-					menuLink.classList.remove('show')
-				}
-			}
-		}
-	})
-})
-
-
-
-
-
-// PROGRESSBAR
-const allProgress = document.querySelectorAll('main .card .progress');
-
-allProgress.forEach(item=> {
-	item.style.setProperty('--value', item.dataset.value)
-})
-
-
-
-
-
-
-// APEXCHART
-var options = {
-  series: [{
-  name: 'series1',
-  data: [31, 40, 28, 51, 42, 109, 100]
-}, {
-  name: 'series2',
-  data: [11, 32, 45, 32, 34, 52, 41]
-}],
-  chart: {
-  height: 350,
-  type: 'area'
-},
-dataLabels: {
-  enabled: false
-},
-stroke: {
-  curve: 'smooth'
-},
-xaxis: {
-  type: 'datetime',
-  categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-},
-tooltip: {
-  x: {
-    format: 'dd/MM/yy HH:mm'
-  },
-},
-};
-
-var chart = new ApexCharts(document.querySelector("#chart"), options);
-chart.render();
+async function fetchData() {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/rdsarjito/nyc_dataset/main/nyc_dataset.json"
+    );
+    const data = await response.json();
+    return data;
+  }
+  
+  function sum(data, key) {
+    return data.reduce((acc, curr) => acc + curr[key], 0);
+  }
+  
+  function range(start, end) {
+    return Array.from({ length: end - start }, (_, i) => start + 1 + i);
+  }
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  
+  function createFilter(data, startDate, endDate) {
+    const uniqueBorough = Array.from(new Set(data.map((item) => item.BOROUGH)));
+  
+    const filteredData = data
+      .map((item) => {
+        const [date, month, year] = item["SALE DATE"]
+          .split("/")
+          .map((val) => Number(val));
+  
+        const convertedDate = new Date(year, month - 1, date);
+  
+        const newItem = {
+          ...item,
+          date: convertedDate,
+          dateValue: { date, month: month - 1, year },
+        };
+        return newItem;
+      })
+      .filter((item) => {
+        return item.date >= startDate && item.date <= endDate;
+      });
+  
+    function getMonthlySales() {
+      const scale = d3
+        .scaleUtc()
+        .domain([startDate, endDate])
+        .ticks(d3.utcMonth.every(1));
+  
+      const formattedScale = scale.map((date) => {
+        return `${monthNames[date.getMonth()]} ${date.getFullYear()}`;
+      });
+  
+      const monthlySales = uniqueBorough.map((borough) => {
+        const sales = scale.map((date) => {
+          const month = date.getMonth();
+          const year = date.getFullYear();
+  
+          const isOnMonthRange = (data) =>
+            data.dateValue.month === month && data.dateValue.year === year;
+  
+          const data = filteredData.filter(
+            (data) => data.BOROUGH === borough && isOnMonthRange(data)
+          );
+  
+          const salesTotal = sum(data, "SALE PRICE");
+  
+          return salesTotal;
+        });
+  
+        return {
+          borough,
+          sales,
+        };
+      });
+  
+      return { data: monthlySales, scales: formattedScale };
+    }
+  
+    return {
+      getMonthlySales,
+      data: filteredData,
+    };
+  }
+  
+  (async function main() {
+    const chart = await fetchData();
+    const startDate = new Date("2016-09-01");
+    const endDate = new Date("2017-08-31");
+    const filter = createFilter(chart, startDate, endDate);
+  
+    console.log(filter.getMonthlySales());
+  
+    createMontlySaleChart(filter);
+  })();
+  
+  function createMontlySaleChart(filter) {
+    const color = [
+      "hsl(24.6 95% 53.1%)",
+      "hsla(180, 40%, 65%, 1)",
+      "#63C7B2",
+      "#3F612D",
+      "#18206F",
+    ];
+  
+    const totalMonthlySalesCtx = document.getElementById("totalMontlySales");
+    const scale = filter.getMonthlySales().scales;
+    const datasets = filter.getMonthlySales().data.map((item, i) => ({
+      label: item.borough,
+      data: item.sales,
+      fill: false,
+      borderColor: color[i % color.length],
+      backgroundColor: color[i % color.length],
+      tension: 0.3,
+    }));
+  
+    new Chart(totalMonthlySalesCtx, {
+      type: "line",
+      data: {
+        labels: scale,
+        datasets: datasets,
+      },
+      options: {
+        aspectRatio: 10 / 4,
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+  
